2a3bf00dcc9f51ee884bb5928e918a0fffbcd685
adji <andres.adjimann@coinfabrik.com>
2020-04-20 13:27:18 -0300

    append the oracle own signature to message

--- servers/oracle/src/oracle_coin_pair_loop.py
+++ servers/oracle/src/oracle_coin_pair_loop.py
@@ -124,6 +124,7 @@ async def gather_signatures(oracles, params, message, my_signature):
                       timeout=oracle_settings.ORACLE_GATHER_SIGNATURE_TIMEOUT)
         for oracle in oracles if oracle.addr != params.oracle_addr]
     sigs = await asyncio.gather(*cors, return_exceptions=True)
+    sigs.append(OracleSignature(params.oracle_addr, my_signature))
     # Sort signatures by addr so the smart contract accept them.
     sorted_sigs = sorted([x for x in sigs if x is not None], key=lambda y: int(y.addr, 16))
     return [x.signature for x in sorted_sigs]
