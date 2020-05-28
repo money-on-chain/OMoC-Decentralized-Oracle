18e72debb67854370a394c5df8751096e69c774b
Leandro Rawicz <leandro.rawicz@coinfabrik.com>
2020-05-28 13:46:41 -0300

    add step03, add modal, fix css, add template

--- markdown/converter.py
+++ markdown/converter.py
@@ -1,21 +1,24 @@
 import pymmd
 import os,re
 
-
+template = "template.html"
 files = ["index","step01", "step02", "step03"]
 htmlpath = "./HTML/"
 
+file = open(template,mode='r')
+templateContent = file.read()
+file.close()
 for file in files:
 	htmlFile = htmlpath + file + ".html" 
 	mmdFile = file + ".mmd"
 
 	if os.path.exists(htmlpath + file): os.remove(htmlFile) 
 	#generate html from mmdFile and return body content from HTML
+	print (mmdFile)
 	html_RAW = pymmd.convert_from(mmdFile)
 	body = re.search(r'<body>.*</body>',html_RAW,re.DOTALL).group(0)[6:-7]
-	html_RAW = re.sub(re.escape("</head>"),
-					'	<link rel="stylesheet" type="text/css" href="style.css" media="screen"/> </head>',
-					html_RAW)
+	body = re.sub(r'<img',r'<img class="img-fluid"',body)
+	html = re.sub("ID_CONTENIDO",body,templateContent)
 	file = open(htmlFile,"w+") 
-	file.write(html_RAW)
+	file.write(html)
 	file.close()
