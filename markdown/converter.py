ffe992ed03d3f8b966ee63b811be86af4c5dd454
Leandro Rawicz <leandro.rawicz@coinfabrik.com>
2020-05-28 15:49:18 -0300

    markdown: fix logos, fix modal, remove "back to index" from index, correct name for img

--- markdown/converter.py
+++ markdown/converter.py
@@ -17,8 +17,13 @@ for file in files:
 	print (mmdFile)
 	html_RAW = pymmd.convert_from(mmdFile)
 	body = re.search(r'<body>.*</body>',html_RAW,re.DOTALL).group(0)[6:-7]
-	body = re.sub(r'<img',r'<img class="img-fluid"',body)
-	html = re.sub("ID_CONTENIDO",body,templateContent)
+	body = re.sub(r'<img',r'<br /> <img class="img-fluid"',body)
+	if file=="index":
+		templateIndex = re.sub(r'<a href="index.*</a>',r'',templateContent,re.DOTALL) if file=="index" else body
+		html = re.sub("ID_CONTENIDO",body,templateIndex)
+	else:
+		html = re.sub("ID_CONTENIDO",body,templateContent)
+
 	file = open(htmlFile,"w+") 
 	file.write(html)
 	file.close()
