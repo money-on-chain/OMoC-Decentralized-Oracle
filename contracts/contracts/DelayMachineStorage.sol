d2ec4e1370c1ccefa0e3e58cbd851fe2a34de6ef
adji <andres.adjimann@coinfabrik.com>
2020-09-01 11:36:41 -0300

    feat(wip): added the delay machine

---
+++ contracts/contracts/DelayMachineStorage.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.6.0;
+
+import {SafeMath} from "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+// prettier-ignore
+import {EnumerableSet} from "@openzeppelin/contracts-ethereum-package/contracts/utils/EnumerableSet.sol";
+import {Initializable} from "@openzeppelin/upgrades/contracts/Initializable.sol";
+import {IERC20} from "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import {Governed} from "@moc/shared/contracts/moc-governance/Governance/Governed.sol";
+import {IGovernor} from "@moc/shared/contracts/moc-governance/Governance/IGovernor.sol";
+import {IStakingMachine} from "@moc/shared/contracts/IStakingMachine.sol";
+
+contract DelayMachineStorage is Initializable, Governed {
+    using SafeMath for uint256;
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    struct Payment {
+        address source;
+        uint256 expiration;
+        uint256 amount;
+    }
+
+    struct Owner {
+        EnumerableSet.UintSet ids;
+    }
+
+    uint256 public _id;
+    IERC20 public _token;
+    mapping(address => Owner) internal owners;
+    mapping(uint256 => Payment) internal payments;
+
+    // Empty internal constructor, to prevent people from mistakenly deploying
+    // an instance of this contract, which should be used via inheritance.
+    constructor() internal {}
+
+    // solhint-disable-previous-line no-empty-blocks
+
+    // Reserved storage space to allow for layout changes in the future.
+    uint256[50] private ______gap;
+}
