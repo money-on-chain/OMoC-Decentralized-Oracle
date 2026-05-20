f438d998b5172d440788593ccf890a57a1b2711f
nubis <nubis@constata.eu>
2026-05-20 10:56:16 +0200

    undoes more bogus CI changes

--- contracts/change/MocRegistryInitChange.sol
+++ contracts/change/MocRegistryInitChange.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.12;
 
-import { ChangeContract } from "@moc/periphery/moc-governance/Governance/ChangeContract.sol";
-import { IRegistry } from "@moc/periphery/IRegistry.sol";
-import { RegistryConstants } from "@moc/periphery/RegistryConstants.sol";
+import { ChangeContract } from "@moc/periphery/contracts/moc-governance/Governance/ChangeContract.sol";
+import { IRegistry } from "@moc/periphery/contracts/IRegistry.sol";
+import { RegistryConstants } from "@moc/periphery/contracts/RegistryConstants.sol";
 
 /**
   @title MocRegistryInitChange
@@ -24,6 +24,7 @@ contract MocRegistryInitChange is ChangeContract, RegistryConstants {
         address _delayMachine,
         address _oracleManager,
         address _supporters,
+        address _infoGetter,
     ) public {
         registry = _registry;
         delayMachine = _delayMachine;
@@ -48,13 +49,20 @@ contract MocRegistryInitChange is ChangeContract, RegistryConstants {
         registry.setUint(ORACLE_PRICE_FETCH_RATE, 5);
         registry.setUint(ORACLE_BLOCKCHAIN_INFO_INTERVAL, 3);
         registry.setUint(ORACLE_COIN_PAIR_LOOP_TASK_INTERVAL, 5);
+        registry.setUint(ORACLE_MAIN_LOOP_TASK_INTERVAL, 120);
+        registry.setDecimal(ORACLE_PRICE_REJECT_DELTA_PCT, 5, 1);
+        registry.setUint(ORACLE_CONFIGURATION_TASK_INTERVAL, 240);
         registry.setUint(ORACLE_GATHER_SIGNATURE_TIMEOUT, 60);
         registry.setUint(ORACLE_MAIN_EXECUTOR_TASK_INTERVAL, 20);
+        registry.setUint(SCHEDULER_POOL_DELAY, 1 * 60);
+        registry.setUint(SCHEDULER_ROUND_DELAY, 30 * 60);
         registry.setUint(ORACLE_PRICE_DIGITS, 18);
         registry.setUint(ORACLE_QUEUE_LEN, 30);
         registry.setUint(MESSAGE_VERSION, 3);
+        registry.setDecimal(ORACLE_PRICE_DELTA_PCT, 5, -2);
         registry.setUint(ORACLE_PRICE_PUBLISH_BLOCKS, 0);
-
+        registry.setBytes(ORACLE_ENTERING_FALLBACKS_AMOUNTS, hex"020406080a");
+        registry.setUint(ORACLE_TRIGGER_VALID_PUBLICATION_BLOCKS, 30);
         // usable just once!!!
         registry = IRegistry(0);
     }
